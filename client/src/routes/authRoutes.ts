96c671c4ec3ce9e89ad1125cedccb9c7af0787e0
belindaTjhin <belindatjhin57@gmail.com>
2025-09-04 00:26:59 +1000

    Add logout and delete features

--- client/src/routes/authRoutes.ts
+++ client/src/routes/authRoutes.ts
@@ -1,9 +1,11 @@
-// routes/authRoutes.ts
-import { Router } from "express";
-import { signup, login } from "../controller/authController";
+import express from "express";
+import { signup, login, logout, deleteAccount } from "../controller/authController";
 
-const router = Router();
+const router = express.Router();
+
 router.post("/signup", signup);
 router.post("/login", login);
+router.post("/logout", logout);
+router.delete("/delete", deleteAccount);
 
 export default router;
