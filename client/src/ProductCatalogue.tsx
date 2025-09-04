b755946d6048e05bc8a088ddfa661dee81faa83b
vinixcollen <vinixcedu@gmail.com>
2025-09-04 12:28:02 +1000

    Added MVC for Product Catalogue feature

---
+++ client/src/ProductCatalogue.tsx
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ProductList from './components/ProductList';
+import ProductDetail from './components/ProductDetail';
+import './ProductCatalogue.css';
+
+function ProductCatalogue() {
+  return (
+    <Router>
+      <div className="ProductCatalogue">
+        <main>
+          <Routes>
+            <Route path="/" element={<ProductList />} />
+            <Route path="/product/:productId" element={<ProductDetail />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
+  );
+}
+
+export default ProductCatalogue;
