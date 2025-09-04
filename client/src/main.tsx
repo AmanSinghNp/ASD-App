8464540fd52f91b4da7ba4889712e3b0252d6401
Danny Leung <danleung1234@gmail.com>
2025-09-04 11:52:55 +1000

    idek changes

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import Checkout from "./Checkout.tsx";
+import Checkout from "./Checkout.tsx"; //changed fromm App
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Checkout />
+    <Checkout /> //changed fromm App
   </StrictMode>
 );
