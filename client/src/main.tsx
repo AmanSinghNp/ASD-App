ce22701c65427bd7e6fa502f61ca2091c1b56680
Danny Leung <danleung1234@gmail.com>
2025-09-11 13:54:43 +1000

    random commit

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import ProductCatalogue from './ProductCatalogue.tsx'; // product catalogue function
+// import ProductCatalogue from './ProductCatalogue.tsx'; // product catalogue function
 import Checkout from "./Checkout.tsx"; //Checkout function
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Checkout /> //changed fromm App
-    <ProductCatalogue /> //render product catalouge
+    {/* <ProductCatalogue /> //render product catalouge */}
   </StrictMode>
 );
-
