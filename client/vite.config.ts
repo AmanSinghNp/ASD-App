f3391eb50b061707d3ca041d8bbf5d2acec0ff86
Xiao Luo <xluo4416@gmail.com>
2025-10-01 15:20:07 +1000

    Add new testing files under _test_

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,20 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // add test here 
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts', 
+  },
 })
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
